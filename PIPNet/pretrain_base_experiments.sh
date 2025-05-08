7d64bc76284ff05251a57ce01e3f9152e6a0d2d5
wlodder <wolodyak@hotmail.co.uk>
2025-05-08 13:56:58 +0100

    add new script

--- PIPNet/pretrain_base_experiments.sh
+++ PIPNet/pretrain_base_experiments.sh
@@ -11,20 +11,16 @@ batch_size=24
 
 # Different sets of prototypes
 
+source ../joint/set_env_var_template.sh janes janes
 for dropout in 0.0 #0.05 0.15 # 0.3 0.5 0.7 0.9
 do
 
-	# for num_proto in 100 200 400 800 1200
-	for num_proto in  400 600 800 
+	for num_proto in  100 200 400 600 800 
 	do
-		# source ../joint/set_env_var_template.sh janes
-		# ../joint/settings/PIP_settings_Fine_Marvel_janes_cutmix_pretrain.sh $num_proto $batch_size $dropout
-		# # Different data set subsets
-		for trial in $(seq 0 2)
-		do 
-			echo $trial
-			source ../joint/set_env_var_template.sh trial_5_${trial}
-			../joint/settings/PIP_settings_Fine_Marvel_janes_cutmix_pretrain.sh $num_proto $batch_size $dropout
+		# ../joint/settings/PIP_settings_Fine_Marvel_janes_pure.sh $num_proto $batch_size $dropout
+		for alpha in 1 2 3 6 8 
+		do
+			../joint/settings/PIP_settings_Fine_Marvel_janes_crp.sh $num_proto $batch_size $dropout $alpha
 		done
 	done
 done
